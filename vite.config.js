023dae533f295e9d3720186799f7d3293d94d4f2
Anuj Kumar Verma <anujvk@outlook.com>
2025-07-22 00:43:34 +0530

    fix: add trailing slash to base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/My-Portfolio/", // <-- MUST have trailing slash and match your repo name exactly!
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
