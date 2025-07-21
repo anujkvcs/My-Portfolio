35d1e1c87db84e1267d972e732ec80833b4fc0a1
Anuj Kumar Verma <anujvk@outlook.com>
2025-07-21 20:39:34 +0530

    Update before deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // ✅ Required for aliasing
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/My-Portfolio",
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // ✅ Alias for "@/..." paths
+    },
+  },
 })
